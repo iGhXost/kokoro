6699eac995b102fb9c188e88ebd6207f963f55b4
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-03-05 01:24:31 +0700

    Update salam.js

--- plugins/salam.js
+++ plugins/salam.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn, text }) => {
 }
 handler.help = ['salam']
 handler.tags = ['salam']
-handler.customPrefix = /(\ $)/
+handler.customPrefix = null
 handler.command = /^assalamualaikum$/i
 handler.owner = false
 handler.mods = false
