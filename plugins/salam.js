2df3a3f9e1b8a5d0af6b56760b1b20140da014b5
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-03-05 01:11:00 +0700

    Add files via upload

---
+++ plugins/salam.js
@@ -0,0 +1,21 @@
+let handler = async (m, { conn, text }) => {
+  conn.reply(m.chat, `
+  Waalaikumsalam Wr. Wb.
+`.trim(), m)
+}
+handler.help = ['salam']
+handler.tags = ['salam']
+handler.customPrefix = /($)/
+handler.command = /^assalamualaikum|samlekom$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+
+module.exports = handler
