11cb8f932b0ba6324262aad558b0756d93ff19a0
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-03-09 12:02:42 +0700

    Update attp.js

--- plugins/attp.js
+++ plugins/attp.js
@@ -6,7 +6,7 @@ let handler  = async (m, { conn, text }) => {
   if (text) conn.sendFile(m.chat, global.API('xteam', '/attp', { file: '', text }), 'attp.webp', '', m, false, { asSticker: true })
   else throw 'Uhm...Teksnya?'
 }
-handler.help = ['attp <teks>']
+handler.help = ['attp <teks>[limit]']
 handler.tags = ['sticker']
 handler.command = /^attp$/i
 handler.owner = false
@@ -19,6 +19,7 @@ handler.admin = false
 handler.botAdmin = false
 
 handler.fail = null
+handler.limit = true
 
 module.exports = handler
 
