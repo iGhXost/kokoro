bc995b8c4a6594ac4a0dc368b4c76629b8dec8cf
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-03-10 18:02:41 +0700

    update igstalk.js

--- plugins/igstalk.js
+++ plugins/igstalk.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, args }) => {
   if (!args[0]) throw 'Uhm...username nya mana?'
   let res = await fetch(global.API('xteam', '/dl/igstalk', {
     nama: args[0]
-  }, 'APIKEY'))
+  }, '0148bbad1a10b740'))
   let json = await res.json()
   if (res.status != 200) throw json
   if (json.result.error) throw json.result.message
