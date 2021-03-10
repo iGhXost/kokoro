480757ba4803e991d2c17cb0d306bd9b28724707
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-03-10 19:07:00 +0700

    update daily.js

--- plugins/daily.js
+++ plugins/daily.js
@@ -13,7 +13,7 @@ handler.mods = false
 handler.premium = false
 handler.group = false
 handler.private = false
-handler.unreg = true
+handler.register = true
 
 handler.admin = false
 handler.botAdmin = false
