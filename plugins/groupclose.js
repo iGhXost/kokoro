c8bb23947b24dc98b19fe62150943f4e4c7102df
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-03-10 23:56:46 +0700

    update groupclose.js

--- plugins/groupclose.js
+++ plugins/groupclose.js
@@ -5,11 +5,9 @@ http://wa.me/6283128734012
 */
 let handler = async (m, { conn, args, usedPrefix }) => {
     if (args[0] === 'open') {
-        conn.reply(m.chat, `*Grup berhasil dibuka!*`, m)
         conn.groupSettingChange(m.chat, GroupSettingChange.messageSend, false)
         conn.fakeReply(m.chat, `*Grup berhasil dibuka!*`, '0@s.whatsapp.net', `Kuriyama`, m.chat)
     } else if (args[0] === 'close') {
-        conn.reply(m.chat, `*Grup berhasil ditutup!*`, m)
         conn.groupSettingChange(m.chat, GroupSettingChange.messageSend, true)
         conn.fakeReply(m.chat, `*Grup berhasil ditutup!*`, '0@s.whatsapp.net', `Kuriyama`, m.chat)
     } else if (args[0] === 'buka') {
