54c30a299896d0df83860e8d18b23410836ad635
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-03-10 19:07:53 +0700

    update textpro.js

--- plugins/textpro.js
+++ plugins/textpro.js
@@ -18,6 +18,7 @@ let handler = async (m, { conn, args: [effect], text: txt }) => {
 handler.help = ['textpro'].map(v => v + ' <effect> <text>|[text2]')
 handler.tags = ['tools']
 handler.command = /^(textpro)$/i
+handler.register = true
 
 module.exports = handler
 
