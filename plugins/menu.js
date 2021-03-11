8f0ff5ab758040f516d2149e1e0d2db5ad1cf623
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-03-11 11:54:19 +0700

    update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -78,8 +78,7 @@ let handler  = async (m, { conn, usedPrefix: _p }) => {
 ┏━━━━「 ${conn.user.name} 」
 ┣ ❖ Hai %name!
 ┃
-┣ ❖ Username @${m.participant.split`@`[0]}
-┣ ❖ Jumlah *%exp XP*
+┣ ❖ *%exp XP*
 ┣ ❖ Tersisa *%limit Limit*
 ┃
 ┣ ❖ Tanggal: *%week %weton, %date*
