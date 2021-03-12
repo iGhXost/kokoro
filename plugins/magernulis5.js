eb006189621e7ba6a6594f42e2232a258f683403
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-03-12 11:52:13 +0700

    update magernulis5.js

--- plugins/magernulis5.js
+++ plugins/magernulis5.js
@@ -15,7 +15,7 @@ handler.tags = ['nulis']
 handler.command = /^magernulis5$/i
 handler.owner = false
 handler.mods = false
-handler.premium = false
+handler.premium = true
 handler.group = false
 handler.private = false
 
