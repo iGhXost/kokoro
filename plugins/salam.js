d1223d5b514248bf64b0459e3eccb725ad4ecbd9
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-03-13 14:49:03 +0700

    Delete salam.js

--- plugins/salam.js
+++
@@ -1,8 +0,0 @@
-let handler = async m => m.reply(`
-Waalaikumsalam Wr. Wb. ${username}
-`.trim()) // Tambah sendiri kalo mau
-handler.help = ['salam']
-handler.tags = ['salam']
-handler.before = Assalamualaikum
-
-module.exports = handler
