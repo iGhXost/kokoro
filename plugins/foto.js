65a32a319e70aed595b8bc5e820d9a2ebe4da63c
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-03-14 04:25:44 +0700

    update foto.js

--- plugins/foto.js
+++ plugins/foto.js
@@ -5,7 +5,6 @@ let { spawn } = require('child_process')
 let handler  = async (m, { conn, args }) => {
   let inputPath ='src/kuriyama.png'
   
-  let ('exit', () => {
     conn.sendFile(m.chat, inputPath, 'kuriyama.png', '404 not found', m)
   })
 }
