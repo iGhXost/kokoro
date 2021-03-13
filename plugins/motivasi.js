ea0619ad1652c97675254a4a8a70c59a1c3fea05
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-03-13 22:53:31 +0700

    Add files via upload

--- plugins/motivasi.js
+++ plugins/motivasi.js
@@ -1,14 +1,15 @@
 let handler  = async (m, { conn }) => {
-  conn.reply(m.chat,`“${pickRandom(global.pantun)}”`, m)
+  conn.reply(m.chat,`“${pickRandom(global.motivasi)}”`, m)
 }
-handler.help = ['pantun']
+handler.help = ['motivasi']
 handler.tags = ['quotes']
-handler.command = /^(pantun)$/i
+handler.command = /^(motivasi)$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
 handler.group = false
 handler.private = false
+handler.register = true
 
 handler.admin = false
 handler.botAdmin = false
@@ -21,18 +22,56 @@ function pickRandom(list) {
   return list[Math.floor(list.length * Math.random())]
 }
 
-global.pantun = [
-" *PANTUN:*\n\nAda anak kecil bermain batu,\nBatu dilempar masuk ke sumur,\nBelajar itu tak kenal waktu,\nJuga tidak memandang umur.\n *Bye Instagram:* @arpunchs",
-" *PANTUN:*\n\nTanam kacang di pagi hari,\nTumbuh enam layu sebatang,\nKeburukan orang jangan dicari,\nBila kalian sedang puasa.\n *Bye Instagram:* @arpunchs",
-" *PANTUN:*\n\nAkhir bulan mendapat gaji,\nGaji untuk membeli ketupat,\nRajin-rajinlah sholat dan mengaji,\nJanganlah lupa puasa dan zakat.\n *Bye Instagram:* @arpunchs",
-" *PANTUN:*\n\nWaktu daftar hari terakhir,\nWaktu terasa banyak terbuang,\nKamu nggak perlu khawatir,\ncintaku hanya untukmu seorang.\n *Bye Instagram:* @arpunchs",
-" *PANTUN:*\n\nAda anak kecil bermain batu,\nBatu dilempar masuk ke sumur,\nBelajar itu tak kenal waktu,\nJuga tidak memandang umur.\n *Bye Instagram:* @arpunchs",
-" *PANTUN:*\n\nSeribu bebek di kandang singa,\nhanya satu berwarna belang,\nBeribu cewek di Indonesia,\nhanya engkau yang aku sayang.\n *Bye Instagram:* @arpunchs",
-" *PANTUN:*\n\nHari minggu pergi ke pasar,\nBeli sayur dan juga beras,\nTiap hari harus belajar,\nPasti akan menjadi cerdas.\n *Bye Instagram:* @arpunchs",
-" *PANTUN:*\n\nAyam goreng setengah mateng,\nBelinya di depan tugu.\nAbang sayang, abangku ganteng,\nlneng di sini setia menunggu.\n *Bye Instagram:* @arpunchs",
-" *PANTUN:*\n\nApi kecil dari tungku,\nApinya kecil habis kayu.\nSudah lama kutunggu-tunggu,\nkapan kamu bilang I love you.\n *Bye Instagram:* @arpunchs",
-" *PANTUN:*\n\nSeribu bebek di kandang singa,\nhanya satu berwarna belang\nBeribu cewek di Indonesia,\nhanya engkau yang aku sayang.\n *Bye Instagram:* @arpunchs",
-" *PANTUN:*\n\nPergi memancing saat fajar,\nPulang siang membawa ikan\nSiapa yang rajin belajar\nJadi orang sukses kemudian.\n *Bye Instagram:* @arpunchs",
-" *PANTUN:*\n\nBeli computer itu biasa\nSupaya belajar jadi semangat\nMari kita belajar puasa\nAgar kita jadi kuat\n *Bye Instagram:* @arpunchs",
-" *PANTUN:*\n\nMinum sekoteng hangat rasanya,\nminum segelas ada yang minta.\nLaki-laki ganteng siapa yang punya?\nBolehkah aku jatuh cinta.\n *ByeInstagram:* @arpunchs",
-]
+global.motivasi = [
+"Jangan bicara, bertindak saja. Jangan katakan, tunjukkan saja. Jangan janji, buktikan saja.",
+"Jangan pernah berhenti melakukan yang terbaik hanya karena seseorang tidak memberi Anda penghargaan."
+"Bekerja saat mereka tidur. Belajar saat mereka berpesta. Hemat sementara mereka menghabiskan. Hiduplah seperti mimpi mereka.",
+"Kunci sukses adalah memusatkan pikiran sadar kita pada hal-hal yang kita inginkan, bukan hal-hal yang kita takuti.",
+"Jangan takut gagal. Ketakutan berada di tempat yang sama tahun depan seperti Anda saat ini.",
+"Jika kita terus melakukan apa yang kita lakukan, kita akan terus mendapatkan apa yang kita dapatkan.",
+"Jika Anda tidak dapat mengatasi stres, Anda tidak akan mengelola kesuksesan."
+"Bersikap keras kepala tentang tujuan Anda dan fleksibel tentang metode Anda.",
+"Kerja keras mengalahkan bakat ketika bakat tidak bekerja keras.",
+"Ingatlah bahwa pelajaran terbesar dalam hidup biasanya dipelajari dari saat-saat terburuk dan dari kesalahan terburuk.",
+"Hidup bukan tentang menunggu badai berlalu, tetapi belajar menari di tengah hujan.",
+"Jika rencananya tidak berhasil, ubah rencananya bukan tujuannya.",
+"Jangan takut kalau hidupmu akan berakhir; takutlah kalau hidupmu tak pernah dimulai.",
+"Orang yang benar-benar hebat adalah orang yang membuat setiap orang merasa hebat.",
+"Pengalaman adalah guru yang berat karena dia memberikan tes terlebih dahulu, kemudian pelajarannya.",
+"Mengetahui seberapa banyak yang perlu diketahui adalah awal dari belajar untuk hidup.",
+"Sukses bukanlah akhir, kegagalan tidak fatal. Yang terpenting adalah keberanian untuk melanjutkan.",
+"Lebih baik gagal dalam orisinalitas daripada berhasil meniru.",
+"Berani bermimpi, tapi yang lebih penting, berani melakukan tindakan di balik impianmu.",
+"Tetapkan tujuan Anda tinggi-tinggi, dan jangan berhenti sampai Anda mencapainya.",
+"Kembangkan kesuksesan dari kegagalan. Keputusasaan dan kegagalan adalah dua batu loncatan paling pasti menuju sukses.",
+"Jenius adalah satu persen inspirasi dan sembilan puluh sembilan persen keringat.",
+"Sukses adalah tempat persiapan dan kesempatan bertemu.",
+"Ketekunan gagal 19 kali dan berhasil pada kesempatam yang ke-20.",
+"Jalan menuju sukses dan jalan menuju kegagalan hampir persis sama.",
+"Sukses biasanya datang kepada mereka yang terlalu sibuk mencarinya.",
+"Jangan tunda pekerjaanmu sampai besok, sementara kau bisa mengerjakannya hari ini.",
+"20 tahun dari sekarang, kau mungkin lebih kecewa dengan hal-hal yang tidak sempat kau lakukan alih-alih yang sudah.",
+"Jangan habiskan waktumu memukuli tembok dan berharap bisa mengubahnya menjadi pintu.",
+"Kesempatan itu mirip seperti matahari terbit. Kalau kau menunggu terlalu lama, kau bisa melewatkannya.",
+"Hidup ini terdiri dari 10 persen apa yang terjadi padamu dan 90 persen bagaimana caramu menyikapinya.",
+"Ada tiga cara untuk mencapai kesuksesan tertinggi: Cara pertama adalah bersikap baik. Cara kedua adalah bersikap baik. Cara ketiga adalah menjadi baik.",
+"Alasan nomor satu orang gagal dalam hidup adalah karena mereka mendengarkan teman, keluarga, dan tetangga mereka.",
+"Waktu lebih berharga daripada uang. Kamu bisa mendapatkan lebih banyak uang, tetapi kamu tidak bisa mendapatkan lebih banyak waktu.",
+"Penetapan tujuan adalah rahasia masa depan yang menarik.",
+"Saat kita berusaha untuk menjadi lebih baik dari kita, segala sesuatu di sekitar kita juga menjadi lebih baik.",
+"Pertumbuhan dimulai ketika kita mulai menerima kelemahan kita sendiri.",
+"Jatuh cinta pada dirimu adalah rahasia pertama menuju kebahagiaan.",
+"Janganlah pernah menyerah ketika Anda masih mampu berusaha lagi. Tidak ada kata berakhir sampai Anda berhenti mencoba.",
+"Kemauan adalah kunci sukses. Orang-orang sukses, berusaha keras apa pun yang mereka rasakan dengan menerapkan keinginan mereka untuk mengatasi sikap apatis, keraguan atau ketakutan.",
+"Janganlah pernah menyerah ketika Anda masih mampu berusaha lagi. Tidak ada kata berakhir sampai Anda berhenti mencoba.",
+"Kemauan adalah kunci sukses. Orang-orang sukses, berusaha keras apa pun yang mereka rasakan dengan menerapkan keinginan mereka untuk mengatasi sikap apatis, keraguan atau ketakutan.",
+"Hal pertama yang dilakukan orang sukses adalah memandang kegagalan sebagai sinyal positif untuk sukses.",
+"Ciri khas orang sukses adalah mereka selalu berusaha keras untuk mempelajari hal-hal baru.",
+"Sukses adalah mendapatkan apa yang kamu inginkan, kebahagiaan menginginkan apa yang kamu dapatkan.",
+"Orang pesimis melihat kesulitan di setiap kesempatan. Orang yang optimis melihat peluang dalam setiap kesulitan.",
+"Keraguan membunuh lebih banyak mimpi daripada kegagalan.",
+"Lakukan apa yang harus kamu lakukan sampai kamu dapat melakukan apa yang ingin kamu lakukan.",
+"Optimistis adalah salah satu kualitas yang lebih terkait dengan kesuksesan dan kebahagiaan daripada yang lain.",
+"Penghargaan paling tinggi bagi seorang pekerja keras bukanlah apa yang dia peroleh dari pekerjaan itu, tapi seberapa berkembang ia dengan kerja kerasnya itu.",
+"Cara terbaik untuk memulai adalah dengan berhenti berbicara dan mulai melakukan.",
+"Kegagalan tidak akan pernah menyusul jika tekad untuk sukses cukup kuat.",
