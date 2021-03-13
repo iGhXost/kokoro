8ba16d6ea2f6118470245c0eb8884efb7844e37e
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-03-14 04:12:13 +0700

    Add files via upload

---
+++ plugins/foto.js
@@ -0,0 +1,30 @@
+let util = require('util')
+let path = require('path')
+let { spawn } = require('child_process')
+
+let handler  = async (m, { conn, args }) => {
+  let inputPath ='src/kertas/kuriyama.png'
+  let outputPath = 'tmp/mirai.jpg'
+  ])
+  .on('error', e => conn.reply(m.chat, util.format(e), m))
+  .on('exit', () => {
+    conn.sendFile(m.chat, outputPath, 'mirai.png', '404 not found', m)
+  })
+}
+handler.help = ['foto']
+handler.tags = ['foto']
+handler.command = /^foto$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+handler.register = true
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+
+module.exports = handler
+
