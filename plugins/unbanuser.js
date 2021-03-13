fc44177d0239bd9ef53c39cd2a1baec313e06cd3
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-03-14 03:09:47 +0700

    update unbanuser.js

--- plugins/unbanuser.js
+++ plugins/unbanuser.js
@@ -9,7 +9,7 @@ let handler = async (m, { conn, text}) => {
     users[who].banned = false
     conn.reply(m.chat, `berhasil unbanned`, m)
 }
-handler.help = ['unban']
+handler.help = ['unban @user']
 handler.tags = ['owner']
 handler.command = /^unban$/i
 handler.rowner = true
