88e9555a7d819d7a7e396408b5d9e61bb1bbf206
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-03-14 14:30:19 +0700

    update bucin.js

--- plugins/bucin.js
+++ plugins/bucin.js
@@ -7,7 +7,7 @@ handler.command = /^(bucin)$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
-handler.group = false
+handler.group = true
 handler.private = false
 handler.register = true
 
