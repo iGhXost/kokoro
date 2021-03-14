ee0ff3dc1276876308f365f4cc4c5eb9328b8109
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-03-14 15:11:40 +0700

    update pantun.js

--- plugins/pantun.js
+++ plugins/pantun.js
@@ -7,7 +7,7 @@ handler.command = /^(pantun)$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
-handler.group = false
+handler.group = true
 handler.private = false
 handler.register = true
 
