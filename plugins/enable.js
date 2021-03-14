46bb9950ae10288a94b99fd6474bacf496d12645
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-03-14 14:39:34 +0700

    update enable.js

--- plugins/enable.js
+++ plugins/enable.js
@@ -37,6 +37,7 @@ ${usedPrefix}disable welcome
 handler.help = ['en', 'dis'].map(v => v + 'able <option>')
 handler.tags = ['group', 'owner']
 handler.command = /^((en|dis)able|(tru|fals)e|(turn)?o(n|ff))$/i
+handler.group = true
 handler.register = true
 
 module.exports = handler
