dd53efa16ccb3a5ae7b213c30dce52df0714b130
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-03-14 15:09:32 +0700

    update motivasi.js

--- plugins/motivasi.js
+++ plugins/motivasi.js
@@ -7,7 +7,7 @@ handler.command = /^(motivasi)$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
-handler.group = false
+handler.group = true
 handler.private = false
 handler.register = true
 
