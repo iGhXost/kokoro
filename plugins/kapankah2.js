96dadbe36515751379d85bae27422ac4997aa610
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-03-14 14:43:49 +0700

    update kapankah2.js

--- plugins/kapankah2.js
+++ plugins/kapankah2.js
@@ -10,7 +10,7 @@ handler.command = /^kapan(kah)?$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
-handler.group = false
+handler.group = true
 handler.private = false
 handler.register = true
 
