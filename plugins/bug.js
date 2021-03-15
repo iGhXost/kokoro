e0468436b1c8a7c59fcde9f98a8e56c07fa2a115
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-03-15 21:54:02 +0700

    update bug.js

--- plugins/bug.js
+++ plugins/bug.js
@@ -4,7 +4,7 @@ let handler = async(m, { conn, text }) => {
     if (!text) return conn.reply(m.chat, 'Silahkan masukkan laporan', m)
     if (text > 300) return conn.reply(m.chat, 'Maaf Teks Terlalu Panjang, Maksimal 300 Teks', m)
     var nomor = m.sender
-    const teks1 = `*[REPORT]*\nNomor : wa.me/${nomor.split("@s.whatsapp.net")[0]}\nPesan : ${text}`
+    const teks1 = `*[REPORT]*\n\nNomor : wa.me/${nomor.split("@s.whatsapp.net")[0]}\nPesan : ${text}`
     conn.sendMessage('62895361677059@s.whatsapp.net', teks1, MessageType.text)
     conn.reply(m.chat, '✔️Masalah telah di laporkan ke owner BOT, laporan palsu/main2 tidak akan ditanggapi.', m)
 }
