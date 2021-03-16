f3cbc61ec7a9acc7f47c9eb4ad03719949560e8f
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-03-16 17:01:22 +0700

    update groupOpenClose.js

--- plugins/groupOpenClose.js
+++ plugins/groupOpenClose.js
@@ -1,41 +1,31 @@
-const { GroupSettingChange } = require('@adiwajshing/baileys')
-/*
-made with love, by ariffb
-http://wa.me/6283128734012
-*/
-let handler = async (m, { conn, args, usedPrefix }) => {
-    if (args[0] === 'open') {
-        conn.groupSettingChange(m.chat, GroupSettingChange.messageSend, false)
-        conn.fakeReply(m.chat, `*Grup berhasil dibuka!*`, '0@s.whatsapp.net', `${conn.user.name} Verified Bot`, m.chat)
-    } else if (args[0] === 'close') {
-        conn.groupSettingChange(m.chat, GroupSettingChange.messageSend, true)
-        conn.fakeReply(m.chat, `*Grup berhasil ditutup!*`, '0@s.whatsapp.net', `${conn.user.name} Verified Bot`, m.chat)
-    } else if (args[0] === 'buka') {
-        conn.groupSettingChange(m.chat, GroupSettingChange.messageSend, false)
-        conn.fakeReply(m.chat, `*Grup berhasil dibuka!*`, '0@s.whatsapp.net', `${conn.user.name} Verified Bot`, m.chat)
-    } else if (args[0] === 'tutup') {
-        conn.groupSettingChange(m.chat, GroupSettingChange.messageSend, true)
-        conn.fakeReply(m.chat, `*Grup berhasil ditutup!*`, '0@s.whatsapp.net', `${conn.user.name} Verified Bot`, m.chat)
-    } else (!args || !args[0]){
-        await conn.updatePresence(m.chat, Presence.composing) 
+let { Presence, GroupSettingChange } = require('@adiwajshing/baileys')
+let handler  = async (m, { conn, args, usedPrefix, command }) => {
+	if(!args || !args[0]) {
+		await conn.updatePresence(m.chat, Presence.composing) 
 		conn.reply(m.chat, `*Format salah! Contoh :*\n\n	*○ ${usedPrefix + command} close*\n	*○ ${usedPrefix + command} open*`, m)
-    }
+	} else if(args[0] == 'open') {
+		conn.groupSettingChange(m.chat, GroupSettingChange.messageSend, false)
+	} else if(args[0] == 'close') {
+		conn.groupSettingChange(m.chat, GroupSettingChange.messageSend, true)
+	} else if(args[0] == 'buka') {
+		conn.groupSettingChange(m.chat, GroupSettingChange.messageSend, false)
+	} else if(args[0] == 'tutup') {
+		conn.groupSettingChange(m.chat, GroupSettingChange.messageSend, true)
+	} else {
+		await conn.updatePresence(m.chat, Presence.composing) 
+		conn.reply(m.chat, `*Format salah! Contoh :*\n\n	*○ ${usedPrefix + command} close*\n	*○ ${usedPrefix + command} open*`, m)
+	} 
 }
-
-handler.help = ['group <open/close>']
+handler.help = ['group *open / close*']
 handler.tags = ['group']
 handler.command = /^(group|grup)$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
-handler.group = true
+handler.group = false
 handler.private = false
-handler.register = true
-
 handler.admin = true
 handler.botAdmin = true
-
 handler.fail = null
-handler.limit = false
-
+handler.exp = 0
 module.exports = handler
