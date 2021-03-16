19a4621cadd3241b4ecb923c707a3f0334444b52
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-03-16 12:02:43 +0700

    update paylimit.js

--- plugins/paylimit.js
+++ plugins/paylimit.js
@@ -21,7 +21,7 @@ let handler = async (m, { conn, text }) => {
     m.reply(`(${-poin} Limit) + (${-pjk} Limit (Pajak 2%)) = ( ${-limit} Limit)`)
     conn.fakeReply(m.chat, `+${poin} Limit`, who, m.text)
 }
-handler.help = ['paylimit @user <amount>']
+handler.help = ['paylimit','limit'].map(v => v + ' <amount>')
 handler.tags = ['xp']
 handler.command = /^(pay)?limit$/
 handler.rowner = true
