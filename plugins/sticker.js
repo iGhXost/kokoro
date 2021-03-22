2718227f007abe00b1b8c585eca8cfddc96b005e
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-03-22 10:02:53 +0700

    Add files via upload

--- plugins/sticker.js
+++ plugins/sticker.js
@@ -1,35 +1,25 @@
 const { MessageType } = require('@adiwajshing/baileys')
-const { sticker, sticker2 } = require('../lib/sticker')
+const { sticker } = require('../lib/sticker')
 let handler  = async (m, { conn, args }) => {
   let stiker = false
   try {
-    let q = m.quoted ? m.quoted : m
-    if (/image/.test(q.mimetype || '')) {
+    let q = m.quoted && !args ? m.quoted : m
+    let mime = (q.msg || q).mimetype || ''
+    if (/image|video/.test(mime)) {
       let img = await q.download()
-      if (!img) throw img
-      stiker = await sticker2(img)
-    } else if (args[0]) stiker = await sticker2(false, args[0])
+      if (!img) throw 'Foto/Video tidak ditemukan'
+      stiker = await sticker(img, false, global.packname, global.author)
+    } else if (args[0]) stiker = await sticker(false, args[0], global.packname, global.author)
   } finally {
     if (stiker) conn.sendMessage(m.chat, stiker, MessageType.sticker, {
       quoted: m
     })
   }
 }
-handler.help = ['stiker (reply media)', 'stiker <url>']
+handler.help = ['stiker (caption|reply media)', 'stiker <url>', 'stikergif (caption|reply media)', 'stikergif <url>']
 handler.tags = ['sticker']
-handler.command = /^s(tiker|ticker)$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
+handler.command = /^s(tic?ker)?(gif)?$/i
 handler.register = true
-
-handler.admin = false
-handler.limit = true
-handler.botAdmin = false
-
-handler.fail = null
 
 module.exports = handler
 
--- plugins/ttp.js
+++ plugins/ttp.js
@@ -1,13 +1,12 @@
-const { sticker2 } = require('../lib/sticker')
+const { sticker } = require('../lib/sticker')
 const { MessageType } = require('@adiwajshing/baileys')
 
-let handler  = async (m, { conn, text }) => {
-  if (text) {
-    let stiker = await sticker2(null, global.API('xteam', '/ttp', { file: '', text }))
-    conn.sendMessage(m.chat, stiker, MessageType.sticker, {
-      quoted: m
-    })
-  }
+let handler = async (m, { conn, text }) => {
+  if (!text) throw 'Tidak ada teks?'
+  let stiker = await sticker(null, global.API('xteam', '/ttp', { file: '', text }), global.packname, global.author)
+  conn.sendMessage(m.chat, stiker, MessageType.sticker, {
+    quoted: m
+  })
 }
 handler.help = ['ttp <teks>']
 handler.tags = ['sticker']
@@ -15,7 +14,7 @@ handler.command = /^ttp$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
-handler.group = true
+handler.group = false
 handler.private = false
 handler.register = true
 
@@ -23,7 +22,6 @@ handler.admin = false
 handler.botAdmin = false
 
 handler.fail = null
-handler.limit = true
 
 module.exports = handler
 
