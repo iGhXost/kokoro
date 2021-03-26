628a6ad0fa2234debb9fe29fb2d084e8459cc6eb
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-03-26 22:57:30 +0700

    Delete botchat.js

--- plugins/botchat.js
+++
@@ -1,6 +0,0 @@
-let handler = m => m.reply('Yaa Aku Disini??\n\nIngin Memulai Bot? Ketik #help atau #menu yaa ;)')
-
-handler.customPrefix = /Bot|Kuriyama/i
-handler.command = new RegExp
-
-module.exports = handler
