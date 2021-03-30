9acb9cf4e316507c9675f7ee6da3bc426f21c810
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-03-30 11:17:28 +0700

    update addprems.js

--- plugins/addprems.js
+++ plugins/addprems.js
@@ -1,5 +1,4 @@
 const { MessageType } = require('@adiwajshing/baileys')
-const fs = require('fs')
 
 let handler = async(m, { conn, text, participants, isPrems }) => {
 let who
@@ -8,8 +7,7 @@ let who
   if (!who) throw 'Tag orang yang akan dijadikan premium!'
  // if (participants.map(v=>v.jid).includes(global.conn.user.jid)) {
   let user = `${who.split("@s.whatsapp.net")[0]}`
-  let up = global.prems.push(user)
-  fs.writeFileSync('./config.js',JSON.stringify(up))
+  global.prems.push(user)
     m.reply(`*Berhasil Add Premium✅*\n\nNomor : wa.me/${who.split("@s.whatsapp.net")[0]}\nExpired : 30 Hari\n\nTerimakasih telah beli Premium!`)
  // } else m.reply('Ada nomor host disini...')
 }
