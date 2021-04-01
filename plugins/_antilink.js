139ea710f624b13ad0fc30da8c8bdf872887de5e
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-04-01 19:49:24 +0700

    update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -1,13 +1,17 @@
 let handler = m => m
 
 let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
-handler.before = m => {
+handler.before = function (m, { user }) {
   if (m.isBaileys && m.fromMe) return true
   let chat = global.DATABASE.data.chats[m.chat]
   let isGroupLink = linkRegex.exec(m.text)
 
-  if (chat.antiLink && isGroupLink) m.reply('_*Jika bukan link group ini, mohon segera dihapus!!*_')
-
+  if (chat.antiLink && isGroupLink) {
+    m.reply('_*Jika bukan link group ini, mohon segera dihapus!!*_')
+    if (!user.isAdmin) return true
+      // this.groupRemove(m.chat, [m.sender])
+    }
+  }
   return true
 }
 
