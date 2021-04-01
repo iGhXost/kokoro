817313c6419d539d6dd9744533a8b1255fedd647
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-04-01 20:50:02 +0700

    update wpanime.js

--- plugins/wpanime.js
+++ plugins/wpanime.js
@@ -3,6 +3,7 @@ const fetch = require('node-fetch')
 
 let handler = async (m, { conn }) => {
     try {
+      await m.reply('Searching...')
         let res = await fetch(global.API('xteam', '/randomimage/wpmobile', {}, 'APIKEY'))
         let img = await res.buffer()
         conn.sendMessage(m.chat, img, MessageType.image, {
@@ -13,8 +14,8 @@ let handler = async (m, { conn }) => {
         throw '_*Owner belum membayar tagihan fitur ini*_'
     }
 }
-handler.help = ['wallpaperanime']
-handler.tags = ['internet']
+handler.help = ['wallpaperanime','wpanime']
+handler.tags = ['weebs']
 handler.command = /^(wallpaper|wp)anime$/i
 handler.premium = true
 handler.register = true
