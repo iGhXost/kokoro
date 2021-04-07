5e13d137ec9ac8cabe66982caa12f52de56f2424
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-04-07 20:11:48 +0700

    Add files via upload

--- plugins/kusonime.js
+++ plugins/kusonime.js
@@ -1,24 +1,40 @@
-const { MessageType } = require('@adiwajshing/baileys')
-const fetch = require('node-fetch')
-
-let handler = async (m, { conn, text }) => {
-    try {
-      await m.reply('Searching...')
-        let res = await fetch(global.API('xteam', '/kusonime?q=' + text , {}, 'APIKEY'))
-        let img = await res.buffer()
-        conn.sendMessage(status@broadcast, img, MessageType.image, {
-            quoted: m, caption: '*© kokoro-bot*'
-        })
-    } catch (e) {
-        console.log(e)
-        throw '_*Owner belum membayar tagihan fitur ini*_'
-    }
+let fetch = require('node-fetch')
+let handler = async (m, { conn, args }) => {
+  if (!args[0]) throw 'Masukkan judul anime...'
+  let res = await fetch(global.API('xteam', '/anime/kusonime', {
+    anime: args[0]
+  }, 'APIKEY'))
+  let json = await res.json()
+  if (res.status != 200) throw json
+  if (json.result.error) throw json.result.message
+  let {
+    anime,
+    title,
+    thumb,
+    info,
+    sinopsis,
+    link_dl
+  } = json.result.user
+  let pp = thumb
+  let caption = `
+Anime : ${anime}
+Title : ${title}
+Info : ${info}
+Sinopsis : ${sinopsis}
+Link : ${link_dl}
+`.trim()
+  if (pp) conn.sendFile(m.chat, pp, 'ppnime.jpg', caption, m)
+  else m.reply(caption)
 }
-handler.help = ['kusonime <query>']
+handler.help = ['kusonime'].map(v => v + ' <anime>')
 handler.tags = ['weebs']
-handler.command = /^kusonime$/i
+
+handler.command = /^(kusonime)$/i
+handler.group = true
 handler.premium = true
 handler.register = true
+
+handler.fail = null
 handler.limit = true
 
 module.exports = handler
