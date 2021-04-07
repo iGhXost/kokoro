bb4e32eb92df7da331b52d3ec1598940b67005c5
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-04-07 21:46:47 +0700

    update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -40,7 +40,7 @@ let handler  = async (m, { conn, usedPrefix: _p }) => {
       'xp': 'Exp & Limit',
       'sticker': 'Sticker',
       'weebs': 'Weebs',
-      'nsfw': 'NSFW 🔞'
+      'nsfw': 'NSFW 🔞',
       'game': 'Game',
       'fun': 'Fun',
       'kerang': 'Kerang Ajaib',
