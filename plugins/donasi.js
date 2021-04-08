72de23c6f776f952f1ae3965b18f1e2c2bf83676
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-04-08 11:11:58 +0700

    update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,18 +1,11 @@
 let handler = async m => m.reply(`
-┏━━━━「 Donasi 」
-┣ ❖ by.U       
-┃ ┗ ❖[085156381082]
-┣ ❖ Tri         
-┃ ┗ ❖[0895361677059]
-┣ ❖ Dana    
-┃ ┗ ❖[0895361677059]
-┣ ❖ GoPay  
-┃ ┗ ❖[0895361677059]
-┣ ❖ OVO      
-┃ ┗ ❖[0895361677059]
-┣ ❖ LinkAja 
-┃ ┗ ❖[0895361677059]
-┗━━━━━━━
+*「 Donasi 」*
+
+by.U [085156381082]
+Dana [0895361677059]
+GoPay [0895361677059]
+OVO [0895361677059]
+LinkAja [0895361677059]
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
 handler.tags = ['info']
