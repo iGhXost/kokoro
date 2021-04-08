0dd8792093d5f555cde285acb441d7bd3e8ae791
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-04-08 17:56:32 +0700

    update 404.js

--- plugins/404.js
+++ plugins/404.js
@@ -1,9 +1,9 @@
 let handler = (m, { usedPrefix, command }) => {
 let name = conn.getName(m.sender)
  m.reply(`
-Maaf ${name},,
+Maaf ${name}
 Perintah *${m.text}*
-tidak terdaftar di ${usedPrefix}menu
+tidak terdaftar di *${usedPrefix}menu*
 `.trim())
 }
 handler.command = new RegExp
