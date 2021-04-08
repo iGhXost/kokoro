eda27fb667c0f468ebfed6ff69fcba9ded6ef23f
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-04-08 17:55:08 +0700

    update tutorial.js

--- plugins/tutorial.js
+++ plugins/tutorial.js
@@ -1,51 +1,11 @@
-let handler  = async (m, { conn, usedPrefix: _p }) => {
-  conn.reply(m.chat, `
-Tutorial Buat Bot WA:
-
-Ketik di *[TERMUX]*
-1) pkg update && pkg upgrade
-2) pkg install git -y
-3) pkg install ffmpeg -y
-4) pkg install imagemagick -y
-5) pkg install nodejs -y
-
-Setelah semuanya terinstall...
-6) git clone https://github.com/Kokoronationz/kokoro
-7) ls
-8) cd kokoro
-
-yang *terakhir*...
-Penginstalannya :)
-9) npm i
-10) node index.js
-
-Tinggal Scan QR code nya
-
-_#Enjoy..._
-
-*Thanks to*
-Nurutomo
-St4rz
-Arya274
-Gimenz.id
-MFarelS
-Nezuko
-`.trim(), m)
+let handler = (m, { usedPrefix, command }) => {
+let name = conn.getName(m.sender)
+ m.reply(`
+Maaf ${name},,
+Perintah *${m.text}*
+tidak terdaftar di ${usedPrefix}menu
+`.trim())
 }
-handler.help = ['tutorial']
-handler.tags = ['info']
-handler.command = /^(tutorial)$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
-handler.register = true
-
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
+handler.command = new RegExp
 
 module.exports = handler
-
